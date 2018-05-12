906089e6d4ecf65021af48a251b0ced5be85ee0a
Jeppe Burchardt <jeppe@burchardt.com>
2018-05-12 19:27:47 +0200

    remove unused css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-import './App.css';
-
 import QuizContainer from './containers/quiz';
 
 class App extends Component {
