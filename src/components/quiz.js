5acfa3068ec0f6b0507f1822ce984b6420fb1e82
Jeppe Burchardt <jeppe@burchardt.com>
2018-05-12 21:51:21 +0200

    animations

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Question from './question';
 import Result from './result';
 import './quiz.css';
 
 const Quiz = ({ question, isQuizComplete, totalQuestions, onAnswer, result, onRestart }) => (
   <div className='quiz'>
-    {question && (
-      <Question
-        question={question}
-        onAnswer={onAnswer}
-      />
-    )}
-    {isQuizComplete && (
-      <Result
-        result={result}
-        totalQuestions={totalQuestions}
-        onRestart={onRestart}
-      />
-    )}
+    <TransitionGroup>
+      {question && (
+        <CSSTransition classNames="quiz-slide" timeout={300} in key={question.id}>
+          <div className="quiz-slide">
+            <Question
+              question={question}
+              onAnswer={onAnswer}
+            />
+          </div>
+        </CSSTransition>
+      )}
+      {isQuizComplete && (
+        <CSSTransition classNames="quiz-zoom" timeout={300} in key={result}>
+          <div className="quiz-zoom">
+            <Result
+              result={result}
+              totalQuestions={totalQuestions}
+              onRestart={onRestart}
+            />
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
   </div>
 );
 
